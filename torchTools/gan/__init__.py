63fef9939f24b712477bd818aab7d7e307aa3aad
wucng <gzcl_wcon@126.com>
2020-04-07 10:42:52 +0800

    Update __init__.py

--- torchTools/gan/__init__.py
+++ torchTools/gan/__init__.py
@@ -1,8 +1,1 @@
-from . import dataProcess
-from . import loss
-from . import network
-from . import optimizer
-# from . import other
-# from . import params
-from . import visual
 from . import learner

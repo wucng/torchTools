d79f0791b69ab04d9b60f1160763cea925abc92c
wucng <gzcl_wcon@126.com>
2020-04-19 11:12:25 +0800

    Update yoloLoss.py

--- torchTools/detection/loss/yoloLoss.py
+++ torchTools/detection/loss/yoloLoss.py
@@ -610,7 +610,7 @@ class YOLOv2Loss(YOLOv1Loss):
         temp_gt_boxes = torch.cat((torch.zeros_like(gt_boxes), gt_boxes), -1)
 
         # IOU阈值
-        iou_thres = 0.5
+        iou_thres = 0.3 #0.5
 
         for i, (y, x) in enumerate(zip(grid_ceil[1], grid_ceil[0])):
             # 按IOU筛选最好的anchor

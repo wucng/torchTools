08111325fa44299c9b4d6a83386a34f227475969
wucng <gzcl_wcon@126.com>
2020-05-04 00:24:19 +0800

    Update __init__.py

--- torchTools/__init__.py
+++ torchTools/__init__.py
@@ -1,3 +1,7 @@
 from . import classify
 from . import gan
 from . import detection
+from . import detectionv2
+
+__all__ = ["classify","gan","detection","detectionv2"]
+# __all__ = [k for k in globals().keys() if not k.startswith("_")]
